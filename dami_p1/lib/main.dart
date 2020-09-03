e3bd3aa6e1d4ceb7f00537fcdec70a9368bae3a1
S J <sanjeeb.lama004@gmail.com>
2020-09-03 21:52:13 +0545

    Read tested

--- dami_p1/lib/main.dart
+++ dami_p1/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -9,128 +12,156 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: FirestoreSlideshow()),
+      // home: Scaffold(body: FirestoreSlideshow()),
+      home: StoriesApp(),
     );
   }
 }
 
-class FirestoreSlideshow extends StatefulWidget {
-  @override
-  _FirestoreSlideshowState createState() => _FirestoreSlideshowState();
-}
-
-class _FirestoreSlideshowState extends State<FirestoreSlideshow> {
-  final PageController ctrl = PageController(viewportFraction: 0.8);
-
-  final FirebaseFirestore db = FirebaseFirestore.instance;
-
-  Stream slides;
-
-  String activeTag = 'favorites';
-
-  // Keep track of current page to avoid unnecessary renders
-  int currentPage = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _queryDb();
-// Set state when page changes
-    ctrl.addListener(() {
-      int next = ctrl.page.round();
-      if (currentPage != next) {
-        setState(() {
-          currentPage = next;
-        });
-      }
-    });
-  }
-
+class StoriesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: slides,
-        initialData: [],
-        builder: (context, AsyncSnapshot snap) {
-          List slideList = snap.data.toList();
-
-          return PageView.builder(
-              controller: ctrl,
-              itemCount: slideList.length + 1,
-              itemBuilder: (context, int currentIdx) {
-                if (currentIdx == 0) {
-                  return _buildTagPage();
-                } else if (slideList.length >= currentIdx) {
-                  // Active page
-                  bool active = currentIdx == currentPage;
-                  return _buildStoryPage(slideList[currentIdx - 1], active);
-                }
-              });
-        });
-  }
-
-  void _queryDb({String tag = 'favorites'}) {
-    // Make a Query
-    Query query = db.collection('stories').where('tags', arrayContains: tag);
-
-    // Map the documents to the data payload
-    slides = query.snapshots().map((list) => list.docs.map((doc) => doc.data));
-
-    // Update the active tag
-    setState(() {
-      activeTag = tag;
-    });
-  }
-
-//Builder Functions
-  _buildStoryPage(Map data, bool active) {
-    // Animated Properties
-    final double blur = active ? 30 : 0;
-    final double offset = active ? 20 : 0;
-    final double top = active ? 100 : 200;
-
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 500),
-      curve: Curves.easeOutQuint,
-      margin: EdgeInsets.only(top: top, bottom: 50, right: 30),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage(data['img']),
-          ),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black87,
-                blurRadius: blur,
-                offset: Offset(offset, offset))
-          ]),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stories Details"),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("stories").snapshots(),
+        builder: (context, snapshot) {
+          return !snapshot.hasData
+              ? Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot data = snapshot.data[index];
+                    Map getDocs = data.data();
+                    return ListTile(
+                      // leading: Image.network(getDocs['img']),
+                      title: Text(getDocs["title"]),
+                    );
+                  });
+        },
+      ),
     );
   }
-
-  _buildTagPage() {
-    return Container(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Your Stories',
-          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-        ),
-        Text('FILTER', style: TextStyle(color: Colors.black26)),
-        _buildButton('favorites'),
-        _buildButton('happy'),
-        _buildButton('sad')
-      ],
-    ));
-  }
-
-  _buildButton(tag) {
-    Color color = tag == activeTag ? Colors.purple : Colors.white;
-    return FlatButton(
-        color: color,
-        child: Text('#$tag'),
-        onPressed: () => _queryDb(tag: tag));
-  }
 }
+// class FirestoreSlideshow extends StatefulWidget {
+//   @override
+//   _FirestoreSlideshowState createState() => _FirestoreSlideshowState();
+// }
+
+// class _FirestoreSlideshowState extends State<FirestoreSlideshow> {
+//   final PageController ctrl = PageController(viewportFraction: 0.8);
+
+//   final FirebaseFirestore db = FirebaseFirestore.instance;
+
+//   Stream slides;
+
+//   String activeTag = 'favorites';
+
+//   // Keep track of current page to avoid unnecessary renders
+//   int currentPage = 0;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _queryDb();
+// // Set state when page changes
+//     ctrl.addListener(() {
+//       int next = ctrl.page.round();
+//       if (currentPage != next) {
+//         setState(() {
+//           currentPage = next;
+//         });
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamBuilder(
+//         stream: slides,
+//         initialData: [],
+//         builder: (context, AsyncSnapshot snap) {
+//           List slideList = snap.data.toList();
+
+//           return PageView.builder(
+//               controller: ctrl,
+//               itemCount: slideList.length + 1,
+//               itemBuilder: (context, int currentIdx) {
+//                 if (currentIdx == 0) {
+//                   return _buildTagPage();
+//                 } else if (slideList.length >= currentIdx) {
+//                   // Active page
+//                   bool active = currentIdx == currentPage;
+//                   return _buildStoryPage(slideList[currentIdx - 1], active);
+//                 }
+//               });
+//         });
+//   }
+
+//   void _queryDb({String tag = 'favorites'}) {
+//     // Make a Query
+//     Query query = db.collection('stories').where('tags', arrayContains: tag);
+
+//     // Map the documents to the data payload
+//     slides = query.snapshots().map((list) => list.docs.map((doc) => doc.data));
+
+//     // Update the active tag
+//     setState(() {
+//       activeTag = tag;
+//     });
+//   }
+
+// //Builder Functions
+//   _buildStoryPage(Map data, bool active) {
+//     // Animated Properties
+//     final double blur = active ? 30 : 0;
+//     final double offset = active ? 20 : 0;
+//     final double top = active ? 100 : 200;
+
+//     return AnimatedContainer(
+//       duration: Duration(milliseconds: 500),
+//       curve: Curves.easeOutQuint,
+//       margin: EdgeInsets.only(top: top, bottom: 50, right: 30),
+//       decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(20),
+//           image: DecorationImage(
+//             fit: BoxFit.cover,
+//             image: NetworkImage(data['img']),
+//           ),
+//           boxShadow: [
+//             BoxShadow(
+//                 color: Colors.black87,
+//                 blurRadius: blur,
+//                 offset: Offset(offset, offset))
+//           ]),
+//     );
+//   }
+
+//   _buildTagPage() {
+//     return Container(
+//         child: Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: [
+//         Text(
+//           'Your Stories',
+//           style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+//         ),
+//         Text('FILTER', style: TextStyle(color: Colors.black26)),
+//         _buildButton('favorites'),
+//         _buildButton('happy'),
+//         _buildButton('sad')
+//       ],
+//     ));
+//   }
+
+//   _buildButton(tag) {
+//     Color color = tag == activeTag ? Colors.purple : Colors.white;
+//     return FlatButton(
+//         color: color,
+//         child: Text('#$tag'),
+//         onPressed: () => _queryDb(tag: tag));
+//   }
+// }
