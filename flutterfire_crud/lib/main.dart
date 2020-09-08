5072514fcad9bf431b656261e73034aae00c060f
S J <sanjeeb.lama004@gmail.com>
2020-09-08 21:29:51 +0545

    Single and array read completed

--- flutterfire_crud/lib/main.dart
+++ flutterfire_crud/lib/main.dart
@@ -1,117 +1,204 @@
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Flutter Fire CRUD")),
+        // body: GetUserName("Hc5kYd8KvBzVcQSEa9mH"),
+        // body: UserInformation(),
+        body: ListPage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ListPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ListPageState createState() => _ListPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ListPageState extends State<ListPage> {
+  Future _data;
+  Future getUsers() async {
+    var firestore = Firestore.instance;
+    firestore.collection("users").getDocuments();
+    QuerySnapshot qn = await firestore.collection("users").getDocuments();
+    return qn.documents;
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  navigateToDetail(DocumentSnapshot users) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => DetailList(users: users)));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _data = getUsers();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Container(
+      child: FutureBuilder(
+        future: _data,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading ...");
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                    title: Text(snapshot.data[index].data['name']),
+                    onTap: () {
+                      navigateToDetail(snapshot.data[index]);
+                    });
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+//Get document according to specific ID - StreamBuilder
+// class GetUserName extends StatelessWidget {
+//   final String documentId;
+
+//   GetUserName(this.documentId);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     CollectionReference users = Firestore.instance.collection('users');
+
+//     return FutureBuilder<DocumentSnapshot>(
+//       future: users.document(documentId).get(),
+//       builder:
+//           (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+//         if (snapshot.hasError) {
+//           return Text("Something went wrong");
+//         }
+
+//         if (snapshot.connectionState == ConnectionState.done) {
+//           // Map<String, dynamic> data = snapshot.data.data();
+//           // return Text("Name: ${data['name']} ${data['job']}");
+//         }
+
+//         return Text("loading");
+//       },
+//     );
+//   }
+// }
+
+//Get information using stream builder
+class UserInformation extends StatelessWidget {
+  BuildContext get context => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection("users").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("Something went wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+          return PageView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.network(
+                        snapshot.data.documents.elementAt(index)['img']),
+                    Text(snapshot.data.documents.elementAt(index)["name"]),
+                    Text(snapshot.data.documents.elementAt(index)["job"]),
+                  ],
+                );
+              });
+        });
+  }
+}
+
+class DetailList extends StatefulWidget {
+  final DocumentSnapshot users;
+
+  DetailList({Key key, @required this.users}) : super(key: key);
+  @override
+  _DetailListState createState() => _DetailListState();
+}
+
+class _DetailListState extends State<DetailList> {
+  // int _count() {
+  //   List<String> tags = widget.users.data["tags"];
+  //   int count = tags.length;
+  //   return count;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("${widget.users.data['name']}"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        children: [
+          Column(children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 3.0,
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: Image.network(widget.users.data["img"]),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              child: Card(
+                child: ListTile(
+                  title: Text(widget.users.data["name"]),
+                  subtitle: Text("Age:" + widget.users.data["job"]),
+                ),
+              ),
             ),
-          ],
-        ),
+            Container(
+                height: 500.0, child: ArrayView(widget.users.data["skills"])),
+          ]),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ArrayView extends StatelessWidget {
+  final List skills;
+  ArrayView(this.skills);
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> children = new List<Widget>();
+    skills.forEach((element) {
+      children.add(Text(element));
+    });
+    return ListView(
+      children: children,
     );
   }
 }
